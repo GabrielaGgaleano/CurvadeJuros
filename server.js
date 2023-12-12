16c51f8a9d194b84af050ead090993546a35887c
gabriela <borboletagabriela@hotmail.com>
2023-12-12 13:26:46 -0300

    teste

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3002;
+const port = 8080;
 
 // Servir arquivos estáticos (HTML, CSS, JS, imagens)
 app.use(express.static(path.join(__dirname, 'public')));
