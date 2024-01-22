1f8e0e99b207e5ae650e4342f490cebf6310f4dc
gabriela <borboletagabriela@hotmail.com>
2024-01-22 11:54:17 -0300

    atualizando grafico

--- file.js
+++ file.js
@@ -34,6 +34,7 @@ const server = http.createServer((req, res) => {
   });
 });
 
+
 const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
